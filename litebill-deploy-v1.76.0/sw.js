b016ec1daf2d45c5d3f1e118bd6babbe7db5bf89
MuYANGLLai <2047840797@qq.com>
2026-08-27 22:18:28 +0800

    Add files via upload

---
+++ litebill-deploy-v1.76.0/sw.js
@@ -0,0 +1,47 @@
+/* 轻账单 Service Worker：缓存优先，离线可用 */
+const CACHE = 'litebill-v1.76.0';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './css/style.css',
+  './js/preset.js',
+  './js/store.js',
+  './js/ui.js',
+  './js/charts.js',
+  './js/views.js',
+  './js/app.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit =>
+      hit || fetch(e.request).then(res => {
+        if (res && res.status === 200) {
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, copy));
+        }
+        return res;
+      }).catch(() => caches.match('./index.html'))
+    )
+  );
+});
